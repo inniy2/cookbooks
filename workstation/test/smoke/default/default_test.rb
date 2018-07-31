354ea75943e135c15125d29ee3572b09e4b02c58
inniy2 <inniy2@gmail.com>
2018-07-31 07:44:46 +0000

    init

---
+++ workstation/test/smoke/default/default_test.rb
@@ -0,0 +1,30 @@
+# # encoding: utf-8
+
+# Inspec test for recipe workstation::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+unless os.windows?
+  # This is an example test, replace with your own test.
+  describe user('root') do
+    it { should exist }
+  end
+end
+
+# This is an example test, replace it with your own test.
+describe port(80) do
+  it { should_not be_listening }
+end
+
+
+# group lab
+describe package 'tree' do
+  it { should be_installed }
+end
+
+# group lab
+describe file ('/etc/motd') do 
+  its('content')  { should match (/Whatever/) }
+  it { should be_owned_by 'root' } 
+end
